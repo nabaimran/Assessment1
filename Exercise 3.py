762ce9a1efe4df56bdc88683ea252e28eed8ccd2
nabaimran <nabaaimrann@gmail.com>
2024-11-24 13:58:12 +0400

    Add files via upload

--- Exercise 3.py
+++ Exercise 3.py
@@ -1,1 +1,30 @@
 # 3. Biography 
+bio = {
+    "name": "Naba Imran",
+    "hometown": "Ajman",
+    "age": 17
+}
+
+print(bio["name"], bio["hometown"], bio["age"], sep="\n")
+
+
+# asking for user input 
+
+name = input("What's your full name? ")
+hometown = input("Where are you from? ")
+
+# checking if age is an integer
+while True:
+        ageinput = input("Enter your age: ")
+        if ageinput.isdigit():  
+            age = int(ageinput)
+            break
+        else:
+            print("That doesn't look like a number. Please try again.")
+
+bio = {"name": name, 
+"hometown": hometown, 
+"age": age}
+
+print(bio["name"], bio["hometown"], bio["age"], sep="\n")
+
